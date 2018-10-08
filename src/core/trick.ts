78122c7976cb889e79cfd28475fb347cee04f6f6
orbiter <maretro@gmx.de>
2018-10-08 23:16:18 +0200

    #10 trick object now holds additional info for each card

--- src/core/trick.ts
+++ src/core/trick.ts
@@ -1,7 +1,26 @@
 import { CardSet } from '@core/cardSet';
+import { Card } from '@core/card';
+
+interface cardInfoDict {
+    [key: number]: any
+}
+
 
 class Trick extends CardSet {
+    private cardInfo: string[] = [];
+    addCard(card: Card, info?:string):void {
+        super.addCard(card);
+        if (info) {
+            const index = this.cards.indexOf(card);
+            this.cardInfo[index] = info;
+        } else {
 
+        }
+    }
+    getInfoForCard(card: Card) {
+        const index = this.cards.indexOf(card);
+        return this.cardInfo[index];
+    }
 }
 
 export { Trick }
--- src/core/unit_tests/test.trick.ts
+++ src/core/unit_tests/test.trick.ts
@@ -4,8 +4,20 @@ import 'mocha'
 import { Trick } from '@core/trick'
 
 describe('Trick', () => {
+    const card_01 = new Card({color: 'blue'});
+    const card_02 = new Card({color: 'green'});
+    const card_03 = new Card({color: 'red'});
+    const trick = new Trick();
     it('should hold cards', () => {
-        const trick = new Trick();
         expect(Card.isNull(trick.getCard(0))).be.true;
+        trick.addCard(card_01);
+        expect(trick.getCard(0)).to.equal(card_01);
+    })
+    it ('should be able to save and return an Id to each card transfer', () => {
+        const id = 'Heinz';
+        trick.addCard(card_02, id);
+        const temp_card = trick.getCard(1);
+        expect(trick.getInfoForCard(temp_card)).to.equal(id);
+
     })
 })
